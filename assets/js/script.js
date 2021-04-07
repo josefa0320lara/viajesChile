4b787356504c5049ae4a7cc6d312e794fd56f087
Josefa <josefa0320lara@gmail.com>
2021-04-06 22:34:45 -0400

    agregue nav, carousel, quienes somos y  destacados

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".nav-link").click(function(event){
+        if(this.hash !== "") {
+            event.preventDefault();
+
+            var hash= this.hash;
+            
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
